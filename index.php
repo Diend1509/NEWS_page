33153ce870e0b55c4d71f09ebf1f5f954775f071
Diend1509 <92193411+Diend1509@users.noreply.github.com>
2022-05-16 06:38:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
-<!-- <!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
-</head>
-<body>
-<a href="">Trang chủ</a>
-<br>
-<a href="signin.php">Đăng nhập </a>
-<a href="signup.php">Đăng ký </a>
-<a href="about_us.html">About us</a>
-
-</body>
-</html> -->
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -23,11 +6,15 @@
 body {font-family: Arial, Helvetica, sans-serif}
 * {box-sizing: border-box;}
 
+body, html {
+  height: 100%;
+  margin: 0;
+}
 .bg-img {
   /* The image used */
   background-image: url("bg.jpg");
-  width: 100%;
-  min-height: 600px;
+  
+  height: 70%;
 
   /* Center and scale the image nicely */
   background-position: center;
@@ -36,12 +23,16 @@ body {font-family: Arial, Helvetica, sans-serif}
   
   /* Needed to position the navbar */
   position: relative;
+
+  color: white;
 }
 
 /* Position the navbar container inside the image */
 .container {
   position: absolute;
-  margin: 20px;
+  margin: 0px;
+  left: 950px;
+  top: 10px;
   width: auto;
 }
 
@@ -65,21 +56,53 @@ body {font-family: Arial, Helvetica, sans-serif}
   background-color: #ddd;
   color: black;
 }
+.a {
+  padding: 14px 16px;
+  font-size: 15px;
+  text-align: center;
+  cursor: pointer;
+  outline: none;
+  color: #fff;
+  background-color: #333;
+  border: none;
+  /*border-radius: 15px;*/
+  box-shadow: 0 9px #999;
+}
+.a:hover {background-color: #ddd}
+
+.a:active {
+  background-color: #3e8e41;
+  box-shadow: 0 5px #666;
+  transform: translateY(4px);
+}
 </style>
 </head>
 <body>
 
-<h1>News page</h1>
-<h2>Học viện công nghệ bưu chính viễn thông</h2>
+
 <div class="bg-img">
+  <h1>News page</h1>
+  <h2>Học viện công nghệ bưu chính viễn thông</h2>
   <div class="container">
     <div class="topnav">
-		<a href="signin.php">Đăng nhập </a>
-		<a href="signup.php">Đăng ký </a>
-		<a href="about_us.html">About us</a>
+  		<a href="signin.php">Đăng nhập </a>
+  		<a href="signup.php">Đăng ký </a>
+  		<a href="about_us.php" ">About us</a>
     </div>
   </div>
 </div>
+
+
+<script>
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+</script>
 
 </body>
 </html>
